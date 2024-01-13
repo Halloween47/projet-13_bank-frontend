bc058544c5851f5aedf19f6ce121b8c09f4d16be
Thomas Leconte <thomasl.dw20@gmail.com>
2024-01-13 12:56:04 +0100

    composant hero OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Hero />
+      </main>
       <Footer />
     </div>
   );
