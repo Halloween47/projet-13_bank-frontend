8988c52c5aad3195093c976cfe07f1a311f3108a
Thomas Leconte <thomasl.dw20@gmail.com>
2024-01-13 13:08:46 +0100

    composant feature OK

---
+++ src/components/featureItem.jsx
@@ -0,0 +1,12 @@
+function FeatureItem(props) {
+    return (
+        <div class="feature-item">
+          <img src={props.image} alt="Chat Icon" class="feature-icon" />
+          <h3 class="feature-item-title">{props.titre}</h3>
+          <p>
+            {props.texte}
+          </p>
+        </div>
+    )
+}
+export default FeatureItem;
